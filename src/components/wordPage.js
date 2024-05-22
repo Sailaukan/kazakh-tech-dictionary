23034178ca19e9c700cc49ae106d2d62af71cac3
Shakhnazar Sailaukan <101112128+Sailaukan@users.noreply.github.com>
2024-05-22 18:15:33 +0500

    added debounce function

--- src/components/wordPage.js
+++ src/components/wordPage.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import classes from "./wordPage.module.css";
 import supabase from "../client";
+import { debounce } from 'lodash';
+
 
 const WordPage = (props) => {
     const [translations, setTranslations] = useState([]);
@@ -69,7 +71,6 @@ const WordPage = (props) => {
         }
     }
 
-
     const handleLike = async (translationID, translationCount) => {
         if (!translationID || !userID) return;
 
@@ -87,7 +88,7 @@ const WordPage = (props) => {
 
         const { data2, error2 } = await supabase
             .from('translations')
-            .update({ count: translationCount+1 })
+            .update({ count: translationCount + 1 })
             .eq('id', translationID);
 
         if (error2) {
@@ -95,9 +96,15 @@ const WordPage = (props) => {
         } else {
             console.log('Inserted like:', data2);
         }
-        
+
         window.location.reload();
     }
+
+    const debouncedHandleLike = debounce(handleLike, 250);
+    const handleLikeDebounced = async (translationID, translationCount) => {
+        debouncedHandleLike(translationID, translationCount);
+    };
+
 
 
     return (
@@ -124,7 +131,7 @@ const WordPage = (props) => {
                                         fill="currentColor"
                                         className="opacity-40 hover:opacity-100 cursor-pointer"
                                         viewBox="0 0 16 16"
-                                        onClick={() => handleLike(translation.id, translation.count)}
+                                        onClick={() => handleLikeDebounced(translation.id, translation.count)}
                                     >
                                         <path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm8.5 9.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z" />
                                     </svg>
